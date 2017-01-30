7b401620fe18632d126df28eb9947b688791975f
bwilfart <benjamin.wilfart@staff.voo.be>
2017-01-30 23:35:58 +0100

    init

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>AZ-GROUP</title>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href="./content/bootstrap.min.css" rel="stylesheet">
+    <link href="./content/font-awesome.min.css" rel="stylesheet">
+    <link href="./content/ekko-lightbox.css" rel="stylesheet">
+    <link href="./content/component.css" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-offset="50" data-target="#main-nav">
+
+    <?php require "./views/nav.html"; ?>
+    <?php require "./views/header.html"; ?>
+    <?php require "./views/main.html"; ?>
+    <?php require "./views/footer.html"; ?>
+
+    <script src="./scripts/jquery-2.0.3.js"></script>
+    <script src="./scripts/bootstrap.min.js"></script>
+    <script src="./scripts/smooth-scrolling.js"></script>
+    <script src="./scripts/ekko-lightbox.js"></script>
+</body>
+
+</html>
---
+++ scripts/smooth-scrolling.js
@@ -0,0 +1,65 @@
+﻿$(function () {
+    
+    $("#main-nav ul li a[href^='#']").on('click', function (e) {
+        e.preventDefault();
+
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 49
+        }, 300, function () {
+            window.location.hash = hash;
+        });
+    });
+
+    $('body').append('<div id="scroll-to-top"><i class="fa fa-arrow-up"/></div>');
+
+    $("#scroll-to-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 200, function() {
+            window.location.hash = "#header";
+        });
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() != 0)
+            $("#scroll-to-top").fadeIn();
+        else
+            $('#scroll-to-top').fadeOut();
+    });
+    
+    /* Parallax */
+    $window = $(window);
+    
+    $('div[data-type="background"]').each(function () {
+        var $scroll = $(this);
+
+        function UpdateBackgroundPosition() {
+            var yPos = -($window.scrollTop() / $scroll.data('speed'));
+            // center
+            yPos -= 62;
+            var coords = '50% ' + yPos + 'px';
+
+            $scroll.css({ backgroundPosition: coords });
+        }
+
+        UpdateBackgroundPosition();
+
+        $(window).scroll(function () {
+            UpdateBackgroundPosition();
+        });
+    });
+
+
+
+
+    /* Resize */
+
+    function updateHeaderHeight() {
+        var height = ($window.height() + 10) + "px";
+
+        $("#header").css({ "min-height": height, "max-height": height });
+    }
+
+    updateHeaderHeight();
+
+    $window.resize(updateHeaderHeight)
+});
